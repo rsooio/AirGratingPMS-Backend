4d1f6cdc8433dc2436636cef48c94073fc644b79
RS0010 <rising__sun@outlook.com>
2022-07-30 21:17:31 +0800

    add unique key

--- model/unit_price/unit_price.sql
+++ model/unit_price/unit_price.sql
@@ -8,6 +8,7 @@ CREATE TABLE `unit_price` (
         INDEX `technology_id_index`(`technology_id`),
     `client_id` INT NOT NULL,
         INDEX `client_id_index`(`client_id`),
+        UNIQUE KEY `unit_price_unique_key`(`enterprise_id`, `workshop_id`, `client_id`, `technology_id`),
     `unit_price` DECIMAL(6,2) NOT NULL,
     `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
