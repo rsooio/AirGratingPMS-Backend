f197cb173934fe708cfccc7b76109548f9aaf479
RS0010 <rising__sun@outlook.com>
2022-08-04 01:17:55 +0800

    update technology model

--- model/technology/technology.sql
+++ model/technology/technology.sql
@@ -6,10 +6,10 @@ CREATE TABLE `technology` (
         INDEX `workshop_id_index`(`enterprise_id`, `workshop_id`),
     `name` VARCHAR(30) NOT NULL,
         UNIQUE KEY `name_unique_index`(`enterprise_id`, `workshop_id`, `name`),
-    `info` VARCHAR(300) NOT NULL DEFAULT "-",
+    `info` VARCHAR(300) NOT NULL DEFAULT "",
     `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `remark` VARCHAR(300) NOT NULL DEFAULT "-",
+    `remark` VARCHAR(300) NOT NULL DEFAULT "",
         -- FULLTEXT INDEX `remark_fulltext_index`(`remark`),
     `version` INT NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
